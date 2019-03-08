d313ce6cc63db6d75f36b0c310e5906f0c08d3b0
kevin_pro <35914036+vendetta546@users.noreply.github.com>
2019-03-08 16:48:43 +0530

    Update pro1.py

--- pro1.py
+++ pro1.py
@@ -1,4 +1,7 @@
-n = int(input('enter a no : '))
-if n<0 :  print('Negative')
-elif n>0 :  print('Positive')
-else :  print('Zero')
+k=int(input())
+if(k<0):
+	   print("Negative")
+elif(k>0):
+       	 print("Positive")
+else:
+	print("Zero")
